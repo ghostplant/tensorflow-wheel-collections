7eb5945de5729a4d354cef18acc1be32a9c66edb
CUI Wei <ghostplant@qq.com>
2018-08-27 09:59:56 +0800

    update versions

--- make-tf18-py35-cuda8-cudnn7-ubuntu.sh
+++ make-tf18-py35-cuda8-cudnn7-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-docker build . --network host -t tensorflow -f "${@:-Dockerfile.tf18-py35-cuda8-cudnn7104-ubuntu}"
+docker build . --network host -t tensorflow -f "${@:-Dockerfile.tf18-py35-cuda8-cudnn7-ubuntu}"
 docker run -it --rm -v `pwd`:/mnt tensorflow bash -c 'cp ../tensorflow_pkg/*.whl /mnt'
 
 echo "You can install the wheel package via:"
